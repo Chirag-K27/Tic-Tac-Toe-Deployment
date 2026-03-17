c83746502d304733e4123e8da5134b7bc66ea930
Chirag Krishnamurthi <chiragkrishnamurthi3005@gmail.com>
2026-03-17 14:09:29 +0530

    fix: replace wget with curl, add .gitignore

--- devops/terraform/jenkins.tf
+++ devops/terraform/jenkins.tf
@@ -44,7 +44,7 @@ resource "aws_instance" "jenkins" {
     dnf install -y java-17-amazon-corretto-devel
 
     echo "===== [3/9] Jenkins ====="
-    wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+    curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
     rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
     dnf install -y jenkins
     systemctl enable jenkins
