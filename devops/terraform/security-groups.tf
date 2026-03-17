e797094044a534bcc0b099bc60901f1af128ef8f
Chirag Krishnamurthi <chiragkrishnamurthi3005@gmail.com>
2026-03-17 16:00:40 +0530

    fix: allow Jenkins to reach EKS cluster

--- devops/terraform/security-groups.tf
+++ devops/terraform/security-groups.tf
@@ -26,6 +26,15 @@ resource "aws_vpc_security_group_ingress_rule" "eks_cluster_https" {
   description                  = "Allow worker nodes to communicate with cluster API"
 }
 
+resource "aws_vpc_security_group_ingress_rule" "eks_cluster_jenkins" {
+  security_group_id            = aws_security_group.eks_cluster.id
+  referenced_security_group_id = aws_security_group.jenkins.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "Allow Jenkins to communicate with cluster API"
+}
+
 resource "aws_vpc_security_group_egress_rule" "eks_cluster_all" {
   security_group_id = aws_security_group.eks_cluster.id
   cidr_ipv4         = "0.0.0.0/0"
